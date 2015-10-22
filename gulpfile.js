31710c0aa4417d9f4cd8aa699b745d92a725aae8
AHN JAE-HA <eu81273@gmail.com>
2015-10-22 23:03:13 +0900

    browser-sync 추가

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,15 @@ var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
+gulp.task('static-server', ['bundle-javascript', 'update-html', 'update-css'], function () {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+});
 
 gulp.task('bundle-javascript', function () {
     return browserify('src/scripts/main.js')
@@ -13,20 +22,26 @@ gulp.task('bundle-javascript', function () {
         .pipe(gulp.dest('dist/scripts/'));
 });
 
-gulp.task('transfer-html', function () {
-    return gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist/'));
+gulp.task('update-javascript', ['bundle-javascript'], function () {
+    return browserSync.reload();
 });
 
-gulp.task('transfer-css', function () {
+gulp.task('update-html', function () {
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('update-css', function () {
     return gulp.src('src/**/*.css')
-        .pipe(gulp.dest('dist/'));
+        .pipe(gulp.dest('dist/'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
-    gulp.watch('src/**/*.css', ['transfer-css']);
-    gulp.watch('src/**/*.html', ['transfer-html']);
-    gulp.watch('src/**/*.js', ['bundle-javascript']);
+    gulp.watch('src/**/*.css', ['update-css']);
+    gulp.watch('src/**/*.html', ['update-html']);
+    gulp.watch('src/**/*.js', ['update-javascript']);
 });
 
-gulp.task('default', ['bundle-javascript', 'transfer-html', 'transfer-css', 'watch']);
+gulp.task('default', ['static-server', 'watch']);
