3aa949835458b025d116e11943a5e0ff5aa6d7df
AHN JAE-HA <eu81273@gmail.com>
2015-10-23 23:42:57 +0900

    browserSync 리로드 방식 변경

--- step4_input/gulpfile.js
+++ step4_input/gulpfile.js
@@ -5,7 +5,7 @@ var buffer = require('vinyl-buffer');
 var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync').create();
 
-gulp.task('static-server', ['bundle-javascript', 'update-html', 'update-css'], function () {
+gulp.task('static-server', ['update-javascript', 'update-html', 'update-css'], function () {
     browserSync.init({
         server: {
             baseDir: "./dist"
@@ -13,29 +13,26 @@ gulp.task('static-server', ['bundle-javascript', 'update-html', 'update-css'], f
     });
 });
 
-gulp.task('bundle-javascript', function () {
+gulp.task('update-javascript', function () {
     return browserify('src/scripts/main.js')
         .bundle()
         .pipe(source('main.js'))
         .pipe(buffer())
         .pipe(uglify())
-        .pipe(gulp.dest('dist/scripts/'));
-});
-
-gulp.task('update-javascript', ['bundle-javascript'], function () {
-    return browserSync.reload();
+        .pipe(gulp.dest('dist/scripts/'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('update-html', function () {
     return gulp.src('src/**/*.html')
         .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('update-css', function () {
     return gulp.src('src/**/*.css')
         .pipe(gulp.dest('dist/'))
-        .pipe(browserSync.stream());
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', function () {
